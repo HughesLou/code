3bda8233017f747104cd52e7a7dbc5ef30da1e6a
Hughes <luzhiwei01@baidu.com>
2017-12-24 23:23:12 +0800

    easy

---
+++ src/main/java/com/hughes/lintcode/easy/MergeIntervals156.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
+ */
+
+package com.hughes.lintcode.easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+import com.hughes.lintcode.model.Interval;
+
+/**
+ * Created by luzhiwei01 on 2017/12/24 22:31.
+ */
+public class MergeIntervals156 {
+    /*
+     * @param intervals: interval list.
+     * @return: A new interval list.
+     */
+    public List<Interval> merge(List<Interval> intervals) {
+        // write your code here
+        List<Interval> result = new ArrayList<>();
+
+        Collections.sort(intervals, new IntervalComparator());
+
+        Interval last = null;
+        for (Interval item : intervals) {
+            if (last == null || last.end < item.start) {
+                result.add(item);
+                last = item;
+            } else {
+                last.end = Math.max(last.end, item.end);
+            }
+        }
+        return result;
+    }
+
+    private class IntervalComparator implements Comparator<Interval> {
+        public int compare(Interval a, Interval b) {
+            return a.start - b.start;
+        }
+    }
+}
---
+++ src/main/java/com/hughes/lintcode/model/Interval.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
+ */
+
+package com.hughes.lintcode.model;
+
+/**
+ * Created by luzhiwei01 on 2017/12/24 22:30.
+ */
+public class Interval {
+    public int start;
+    public int end;
+
+    Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
